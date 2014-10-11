ddcd528acf22cc48044aa669b643290fe4a47784
Konrad Paziewski <kp306410@students.mimuw.edu.pl>
2014-10-11 13:24:59 +0200

    Hello world

---
+++ Cthulhu.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Cthulhu.cpp
+// Author      : konrad.paziewski@gmail.com
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main(void) {
+	std::cout << "fdsfads";
+	puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}
